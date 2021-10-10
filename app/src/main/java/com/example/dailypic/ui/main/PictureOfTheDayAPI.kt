e88e7d1e3c5b8b11a7bf1e952fbe987d827889c1
Alex <69519924+Lovkost@users.noreply.github.com>
2021-10-10 14:46:16 +0300

    BottomNavigation

--- app/src/main/java/com/example/dailypic/ui/main/PictureOfTheDayAPI.kt
+++ app/src/main/java/com/example/dailypic/ui/main/PictureOfTheDayAPI.kt
@@ -7,5 +7,7 @@ import retrofit2.http.Query
 interface PictureOfTheDayAPI {
     @GET("planetary/apod")
     fun getPictureOfTheDay(@Query("api_key") apiKey: String): Call<PODServerResponseData>
+    @GET("planetary/apod")
+    fun getEarthPicture(@Query("api_key") apiKey:String):Call<PODServerResponseData>
 
 }
--- app/src/main/java/com/example/dailypic/ui/main/PictureOfTheDayFragment.kt
+++ app/src/main/java/com/example/dailypic/ui/main/PictureOfTheDayFragment.kt
@@ -1,5 +1,6 @@
 package com.example.dailypic.ui.main
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -11,9 +12,11 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import coil.api.load
 import com.example.dailypic.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.main_activity.*
 import kotlinx.android.synthetic.main.main_fragment.*
 import kotlin.concurrent.fixedRateTimer
-
+const val IS_BOOTOM_NAVIGATION_VIEW = "NAV_VIEW"
 class PictureOfTheDayFragment : Fragment() {
     //Ленивая инициализация модели
     private val viewModel: PictureOfTheDayViewModel by lazy {
@@ -25,8 +28,10 @@ class PictureOfTheDayFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         val view = inflater.inflate(R.layout.main_fragment, container, false)
-        val toolbar = view.findViewById<Toolbar>(R.id.toolbarMain)
-        toolbar.setOnMenuItemClickListener {
+        val bottomNavigation = activity?.findViewById<BottomNavigationView>(R.id.bottomNavigation)
+        val toolbar = activity?.findViewById<Toolbar>(R.id.toolbarSettting)
+//        bottomNavigation?.selectedItemId = getItem(IS_BOOTOM_NAVIGATION_VIEW)
+        toolbar?.setOnMenuItemClickListener {
             when (it.itemId) {
                 R.id.setting -> {
                     fragmentManager?.beginTransaction()?.replace(R.id.container, SettingsFragment())
@@ -36,13 +41,42 @@ class PictureOfTheDayFragment : Fragment() {
             }
             return@setOnMenuItemClickListener true
         }
+        bottomNavigation?.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.DailyPicBNV -> {
+                    savePos(it.itemId)
+                    fragmentManager?.beginTransaction()
+                        ?.replace(R.id.container, PictureOfTheDayFragment())?.addToBackStack(null)
+                        ?.commit()
+                }
+                R.id.EarthPicBNV->{
+                    savePos(it.itemId)
+                    fragmentManager?.beginTransaction()?.replace(R.id.container,EarthPictureFragment())?.addToBackStack(null)?.commit()
+                }
+            }
+            return@setOnNavigationItemSelectedListener true
+        }
         return view
+    }
+
+    private fun getItem(isBootomNavigationView: String): Int {
+ val sharedPrefBottomNavPos = activity?.getPreferences(Context.MODE_PRIVATE)
+        if(sharedPrefBottomNavPos!=null) return sharedPrefBottomNavPos.getInt(isBootomNavigationView,0)
+        return 0
+    }
+
+    private fun savePos(itemId: Int) {
+        var sharedPrefBottomNavPos = activity?.getPreferences(Context.MODE_PRIVATE)
+        val editor = sharedPrefBottomNavPos?.edit()
+        editor?.putInt(IS_BOOTOM_NAVIGATION_VIEW,itemId)
+        editor?.apply()
+
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel.getData()
-            .observe(this@PictureOfTheDayFragment, Observer<PictureOfTheDayData> { renderData(it) })
+            .observe(viewLifecycleOwner, Observer<PictureOfTheDayData> { renderData(it) })
     }
 
     private fun renderData(data: PictureOfTheDayData) {
@@ -54,10 +88,6 @@ class PictureOfTheDayFragment : Fragment() {
                 if (url.isNullOrEmpty()) {
                     toast("Пусто")
                 } else {
-                    //Отобразите фото
-                    //showSuccess()
-                    //Coil в работе: достаточно вызвать у нашего ImageView
-                    //нужную extension-функцию и передать ссылку и заглушки для placeholder
                     image_view.load(url) {
                         lifecycle(this@PictureOfTheDayFragment)
                         error(R.drawable.ic_baseline_error_24)
